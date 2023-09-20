39d89c68aa989abef6c42fdf3f201e0858d02304
joecruz17 <joejcruz17@gmail.com>
2023-09-20 12:29:57 -0400

    saving changes

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,13 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom'
 import Layout from '../components/Layout'
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-      <Route path ="/" element={<Layout />} ></Route>
+      <Route path ="/" element={<Layout />} />
       </Routes>
    
     </div>
