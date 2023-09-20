37e49896cd7d7ce7791836e65f41d8c3688f0e77
joecruz17 <joejcruz17@gmail.com>
2023-09-20 10:34:47 -0400

    some logo and sidebar progress

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import './Sidebar.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo-sub.png'
+
+
+const Sidebar = () => (
+
+<div className='nav-bar'>
+    <Link className='logo' to='/'>
+        <img src={LogoS} alt="logo" />
+        <img src={LogoSubtitle} alt="slobodan" />
+    </Link>
+
+
+</div>
+)
+export default Sidebar
